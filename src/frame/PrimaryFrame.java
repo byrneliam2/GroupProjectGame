659184d476ca96655c431715a2571c40fd596ecd
Liam <liam.jb1@gmail.com>
2017-09-04 14:11:19 +1200

    Start to refactor package for game purposes

---
+++ src/frame/PrimaryFrame.java
@@ -0,0 +1,16 @@
+package frame;
+
+/*
+ * Liam: 300338518
+ * 4/09/2017
+ * SWEN222PROJECT
+ */
+
+/**
+ * The PrimaryFrame represents the master frame of the application...
+ */
+public class PrimaryFrame {
+
+    //
+
+}
