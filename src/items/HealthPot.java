d95ad968301830e9b4614b9167d21de91fd7480b
edwardthom <edwardthom@tasting-room.ecs.vuw.ac.nz>
2017-09-15 14:48:53 +1200

    added HealthPot item

---
+++ src/items/HealthPot.java
@@ -0,0 +1,25 @@
+package items;
+
+import java.awt.Image;
+
+public class HealthPot implements Usable {
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
