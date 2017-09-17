0f7a107f78d852edffe850eb1cd20b802a5d8b0f
James Watt <james.watt529@hotmail.co.nz>
2017-09-17 15:09:43 +1200

    Commenting and completing map skeleton code

---
+++ src/JamesPackage/Enviroment.java
@@ -0,0 +1,13 @@
+package JamesPackage;
+
+/**
+ * This class represents a type of enviroment that a tile in the map can be. For
+ * example a mud enviroment. Each enviroment might affect the player in
+ * different ways.
+ * 
+ * @author James
+ *
+ */
+public enum Enviroment {
+
+}
--- src/JamesPackage/Map.java
+++ src/JamesPackage/Map.java
@@ -16,17 +16,31 @@ import items.Item;
  *
  */
 public class Map {
-
+	// HashMap of the Items located on the map and their locations
 	private HashMap<Item, Location> items;
-	private ArrayList<ArrayList<BufferedImage>> map;
-	private int width;
-	private int height;
-	private int backgroundLayer = 2;
-	private int collisionLayer = 0;
-	private int itemLayer = 3;
-	private int enviromentalLayer = 1;
 
-	public Map(String name, ArrayList<Item> items) {
+	// The width of the map
+	private int width;
+
+	// The height of the map
+	private int height;
+
+	// 2D array of all the images that make up the backGround layer
+	private ArrayList<ArrayList<BufferedImage>> backgroundLayer;
+
+	// 2D array of all the images that make up the collision layer
+	private ArrayList<ArrayList<BufferedImage>> collisionLayer;
+
+	// 2D array of all the images that make up the item layer
+	private ArrayList<ArrayList<BufferedImage>> itemLayer;
+
+	// 2D array of all the images that make up the enviromental layer
+	private ArrayList<ArrayList<BufferedImage>> enviromentalLayer;
+
+	// All the doors located on this map
+	private ArrayList<Door> doors;
+
+	public Map(String name, ArrayList<Item> items, ArrayList<String> doors) {
 
 	}
 
@@ -41,12 +55,19 @@ public class Map {
 
 	/**
 	 * This method splits each layer into the 32x32 tiles and inserts them into the
-	 * map field, as well as sets the width and height of the map
+	 * map field, as well as sets the width and height of the map.
 	 * 
 	 * @param name
 	 *            The name of the map
 	 */
 	private void loadMap(String name) {
+
+	}
+
+	/**
+	 * Initialis's all the entities on the map
+	 */
+	private void loadEntities() {
 
 	}
 
@@ -57,20 +78,74 @@ public class Map {
 	 * @param x
 	 * @param y
 	 */
-	public void canMove(int x, int y) {
-
+	public boolean canMove(int x, int y) {
+		return false;
 	}
 
 	/**
-	 * @return An ArrayList of the images that need to be rendered
+	 * This method returns the 2D ArrayList of images that make up the background
+	 * 
+	 * @return A 2D ArrayList of buffered images
 	 */
-	public ArrayList<ArrayList<BufferedImage>> getImagesToRender() {
-		return new ArrayList<ArrayList<BufferedImage>>(
-				Arrays.asList(this.map.get(this.backgroundLayer), this.map.get(this.itemLayer)));
+	public ArrayList<ArrayList<BufferedImage>> getBackgroundLayer() {
+		return this.backgroundLayer;
 	}
 
-	public void getBackgroundTile() {
+	/**
+	 * This method returns a 2D ArrayList of images that make up the item layer
+	 * 
+	 * @return A 2D ArrayList of buffered images
+	 */
+	public ArrayList<ArrayList<BufferedImage>> getImageLayer() {
+		return this.itemLayer;
+	}
 
+	/**
+	 * This method returns the enviroment on a given tile specified by a x and y
+	 * value, returns null if there is no item. Returns an exception if the x or y
+	 * position is invalid.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return The enviroment on the tile at x,y
+	 */
+	public Enviroment onEnviromentTile(int x, int y) {
+		return null;
+	}
+
+	/**
+	 * This method returns whether there is a item on a given spot
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean isItem(int x, int y) {
+		return false;
+	}
+
+	/**
+	 * This method returns the item located at position x,y. Returns null if there
+	 * is no item. Returns an exception if the x or y position is invalid.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public Item itemAt(int x, int y) {
+		return null;
+	}
+
+	/**
+	 * This method returns the image at a given x and y position. Returns an
+	 * exception if the x and y position is invalid.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public BufferedImage imageAt(int x, int y) {
+		return null;
 	}
 
 }
