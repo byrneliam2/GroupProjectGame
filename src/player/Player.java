2c3d744392dfc733e60025ae26ba3a04b22bd906
Mohsen Javaher <javahemohs@ecs.vuw.ac.nz>
2017-09-19 10:17:45 +1200

    Update Player.java Need to talk to Thomas about this tho

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,5 +1,33 @@
 package player;
 
 public class Player {
+/**
+ * Created by javahemohs on 19/09/17.
+ */
+public class Player {
+    private String name;
+    private List<Item> itemCollection = new ArrayList<Item>();
+    private int power;
+    private int health;
 
+
+
+    public Player(String name){
+        health = 5;
+        power = 1;
+    }
+    /*
+    *@ Returns the name of this Player
+    */
+    public String getName(){
+        return this.name;
+    }
+
+    /*
+    *@ Sets the name of this Player to
+    */
+    public void setName(String name){
+        this.name = name;
+    }
+}
 }
