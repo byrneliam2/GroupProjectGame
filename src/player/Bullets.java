1b5d37da1085a9695d44bca5d8c1a8223c2ae593
Michael JWinton <mohse.javaher@gmail.com>
2017-09-19 11:57:26 +1200

    For the boiiss :)

---
+++ src/player/Bullets.java
@@ -0,0 +1,8 @@
+package player;
+
+public class Bullets {
+
+	public Bullets() {
+
+	}
+}
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,25 +1,94 @@
+package player;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+import items.Backpack;
+import items.DoorItem;
+
+
+
 /**
  * Created by javahemohs on 19/09/17.
  */
 public class Player {
     private String name;
-    private List<Item> itemCollection = new ArrayList<Item>();
-    private int power;
+    private Backpack itemsList = new Backpack(this);
     private int health;
+    private int xLocation;
+    private int yLocation;
+    private Map map;
+    private List<Bullets> bullets = new ArrayList<Bullets>();
 
 
-    public Player(String name){
-        health = 5;
-        power = 1;
+    public Player(String name, int xLocation, int yLocation){
+        this.name = name;
+        this.health = 5;
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+     
+
     }
 
 
-    public void updateFromCollection(){}
-    public void DeleteFromCollection(){}
-    public void AddToCollection(){}
 
-    public boolean canOpenDoor(){return true;}
-    public boolean canMakeMove(){return true;}
+
+
+	public void pickUpItems(){
+    	if(true) {
+
+    	}
+
+    }
+    public void removeItems(){}
+    public void equipItem(){}
+    public void unequipItem() {}
+    public void useItem() {}
+    public void pickUpAndUse() {}
+
+    public boolean canOpenDoor(DoorItem doorItem){
+    	if(itemsList.checkDoorID(doorItem.getDoorID())) {
+    		return true;
+    	}
+    	return false;
+    }
+
+    private boolean canMakeMove(){
+    	// Ask the map if it possible to move (((Later)))
+    	if(true) {
+    		return true;
+    	}
+    	return false;
+    }
+
+    public void move() {
+    	//Check if you can make the move and then update the x and y.
+
+    }
+
+    public void shoot() {
+    	// Make
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /*
     * Returns a String
@@ -34,4 +103,35 @@ public class Player {
     public void setName(String name){
         this.name = name;
     }
+
+    public Backpack getItemsList() {
+		return itemsList;
+	}
+
+
+	public void setItemsList(Backpack itemsList) {
+		this.itemsList = itemsList;
+	}
+
+
+	public int getHealth() {
+		return health;
+	}
+
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+
+	public int getxLocation() {
+		return xLocation;
+	}
+
+
+	public int getyLocation() {
+		return yLocation;
+	}
+
+
 }
