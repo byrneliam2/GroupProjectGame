259903e9aac48b69fc7f810d7037e5be9b83977b
byrneliam2 <byrneliam2>
2017-09-19 15:54:18 +1200

    Update text files

--- src/frames/cards/MenuCard.java
+++ src/frames/cards/MenuCard.java
@@ -10,6 +10,7 @@ import gfx.GraphicsUtilities;
 import gfx.ImageLoader;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * The MenuCard represents the main menu of the game. This contains buttons that start,
@@ -27,9 +28,11 @@ public class MenuCard extends Card {
         // add the components in a top to bottom order, adding glue where we want space
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         panel.add(Box.createVerticalGlue());
+        panel.add(Box.createVerticalGlue());
         //panel.add(GraphicsUtilities.produceSticker(ImageLoader.image("pause.jpg")));
         panel.add(GraphicsUtilities.produceButton(ImageLoader.image("bu_exit.jpg"),
                 ImageLoader.image("pause.jpg"), true));
+        //panel.add(Box.createRigidArea(new Dimension(100, 50)));
         panel.add(GraphicsUtilities.produceButton(ImageLoader.image("bu_exit.jpg"),
                 ImageLoader.image("pause.jpg"), true));
         panel.add(Box.createVerticalGlue());
