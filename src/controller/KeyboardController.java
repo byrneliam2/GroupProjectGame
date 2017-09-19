1acd4c09d29eb1d7e88cd1598ea5cb5bdf0d57e6
mcmanaandr <andrew.mcmanaway>
2017-09-19 16:00:43 +1200

    Keyboard Commands Checked

--- src/controller/KeyboardController.java
+++ src/controller/KeyboardController.java
@@ -3,21 +3,34 @@ package controller;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import static controller.KeyboardController.KeyboardCommands.*;
+
 public class KeyboardController extends KeyAdapter {
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        super.keyReleased(e);
-
-    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         super.keyPressed(e);
+        toggleState(e.getKeyCode(), true);
 
     }
 
-    public enum KeyboardCommands {
+    @Override
+    public void keyReleased(KeyEvent e) {
+        super.keyReleased(e);
+        toggleState(e.getKeyCode(), false);
+    }
+
+
+    private void toggleState(int keybind, boolean state){
+        if(keybind == KEY_UP.getKeybind());
+        else if(keybind == KEY_DOWN.getKeybind());
+        else if(keybind == KEY_LEFT.getKeybind());
+        else if(keybind == KEY_RIGHT.getKeybind());
+        else if(keybind == KEY_USE.getKeybind());
+        else if(keybind == KEY_ATTACK.getKeybind());
+    }
+
+     enum KeyboardCommands {
         KEY_UP(KeyEvent.VK_UP),
         KEY_DOWN(KeyEvent.VK_DOWN),
         KEY_LEFT(KeyEvent.VK_LEFT),
@@ -32,7 +45,7 @@ public class KeyboardController extends KeyAdapter {
         }
 
         public int getKeybind() {
-            return keybind;
+            return this.keybind;
         }
 
         public void setKeybind(int keybind) {
