5a0eed7ad5b33df0525baa260fa3f5587a34fa84
Michael JWinton <mohse.javaher@gmail.com>
2017-09-19 15:47:13 +1200

    updated comments

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,16 +1,26 @@
 package player;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
 import JamesPackage.Map;
 import items.Backpack;
 import items.DoorItem;
+import items.Equipable;
+import items.InvalidBackpackException;
+import items.Item;
+import items.Usable;
 
 /**
+ * @author javahemohs
  * Created by javahemohs on 19/09/17.
+ *
  */
 public class Player {
+	// need to implement the bounding box for the player when the player is making a move
+	// the I have to change the bounding box of the player.
+	private Rectangle boundingBox = new Rectangle (Map.tileWidth, Map.tileHeight);
 	private String name;
 	private Backpack itemsList = new Backpack(this);
 	private int health;
@@ -18,6 +28,11 @@ public class Player {
 	private int yLocation;
 	private Map map;
 
+	/**
+	 * @param name
+	 * @param xLocation
+	 * @param yLocation
+	 */
 	public Player(String name, int xLocation, int yLocation) {
 		this.name = name;
 		this.health = 5;
@@ -26,35 +41,104 @@ public class Player {
 
 	}
 
-	public void pickUpItems() {
-		if (true) {
+	/**
+	 * Pick up an item and put it into the list of BackPack
+	 * @param item
+	 * @throws InvalidPlayerExceptions
+	 */
+	public void pickUpItems(Item item) throws InvalidPlayerExceptions {
+		try {
+			itemsList.pickUpItem(item);
 
+		} catch (InvalidBackpackException e) {
+			throw new InvalidPlayerExceptions(e.getMessage());
+		}
+	}
+
+
+	/**
+	 * Remove the item from the BackPack list
+	 * @param item
+	 * @throws InvalidPlayerExceptions
+	 */
+	public void removeItems(Item item) throws InvalidPlayerExceptions {
+		try {
+			itemsList.removeItem(item);
+		} catch (InvalidBackpackException e) {
+			throw new InvalidPlayerExceptions(e.getMessage());
 		}
 
 	}
 
-	public void canRemoveItems() {
+
+	/**
+	 * equip the item from the BackPack list
+	 * @param item
+	 * @throws InvalidPlayerExceptions
+	 */
+	public void equipItem(Equipable item) throws InvalidPlayerExceptions {
+		try {
+			itemsList.equipItem(item);
+		} catch (InvalidBackpackException e) {
+			throw new InvalidPlayerExceptions(e.getMessage());
+		}
 	}
 
-	public void canEquipItem() {
+	/**
+	 * unequip the item from the list
+	 * @param item
+	 * @throws InvalidPlayerExceptions
+	 */
+	public void unequipItem(Equipable item) throws InvalidPlayerExceptions {
+		try {
+			itemsList.unequipItem(item);
+		} catch (InvalidBackpackException e) {
+			throw new InvalidPlayerExceptions(e.getMessage());
+
+		}
 	}
 
-	public void canUnequipItem() {
+	/**
+	 * check if there is an item in the BackPack List
+	 * @param item
+	 * @throws InvalidPlayerExceptions
+	 */
+	public void useItem(Usable item) throws InvalidPlayerExceptions {
+		try {
+			itemsList.useItem(item);
+		} catch (InvalidBackpackException e) {
+			throw new InvalidPlayerExceptions(e.getMessage());
+
+		}
 	}
 
-	public void canUseItem() {
-	}
+	/**
+	 * pickUpAndUse the item from the BackPack list
+	 * @param item
+	 * @throws InvalidPlayerExceptions
+	 */
+	public void pickUpAndUse(Usable item) throws InvalidPlayerExceptions {
+		try {
+			itemsList.pickUpAndUse(item);
+		} catch (InvalidBackpackException e) {
+			throw new InvalidPlayerExceptions(e.getMessage());
 
-	public void canPickUpAndUse() {
+		}
 	}
 
 	public void checkHit() {
-		// checks whether any bullets that don't belong to 'this' player have hit this player
+		// checks whether any bullets that don't belong to 'this' player have hit this
+		// player
 		// if it has hit the player, it should delete the bullet from the bullet list.
 		// and player should lose health
 		return;
 	}
 
+	/**
+	 * If it is possible for a player to open a door.
+	 * @param doorItem
+	 * @return
+	 */
 	public boolean canOpenDoor(DoorItem doorItem) {
 		if (itemsList.checkDoorID(doorItem.getDoorID())) {
 			return true;
