859943b2fb60354311b65d5a9206d1999e1b465f
McManaway1 <andrew.mcmanaway@gmail.com>
2017-09-21 15:17:40 +1200

    LabelButton Test

---
+++ src/gfx/tests/LabelButton.java
@@ -0,0 +1,57 @@
+package gfx.tests;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class LabelButton extends JButton{
+    private String text;
+    private final int WIDTH, HEIGHT;
+
+    public LabelButton(String text, int width, int height, boolean rollover) {
+        this.setContentAreaFilled(false);
+        this.setBorderPainted(false);
+        this.setAlignmentX(0.5f);
+        this.text = text;
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        this.createImages(rollover);
+    }
+
+    private void createImages(boolean rollover){
+        BufferedImage main = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = main.createGraphics();
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        g2d.setRenderingHints(rh);
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.white);
+        g2d.drawRect(0,0,WIDTH,HEIGHT);
+
+        Font font = new Font("Gill Sans MT Condensed", Font.BOLD, 32);
+        FontMetrics metrics = g2d.getFontMetrics(font);
+        g2d.setFont(font);
+
+        int x = (WIDTH - metrics.stringWidth(this.text)) / 2;
+        int y = ((HEIGHT - metrics.getHeight()) / 2) + metrics.getAscent();
+
+        g2d.drawString(this.text, x, y);
+        this.setIcon(new ImageIcon(main));
+
+        if(rollover){
+            BufferedImage roll = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+            g2d = roll.createGraphics();
+            g2d.setRenderingHints(rh);
+            g2d.setColor(Color.white);
+            g2d.fillRect(0,0,WIDTH,HEIGHT);
+
+            g2d.setColor(Color.BLACK);
+            g2d.setFont(font);
+            g2d.drawString(this.text, x, y);
+            this.setRolloverEnabled(true);
+            this.setRolloverIcon(new ImageIcon(roll));
+        }
+    }
+}
