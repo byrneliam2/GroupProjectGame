2a8834d2f4cca3e7d8409f946412437a4722cd7a
Liam <liam.jb1@gmail.com>
2017-09-24 20:22:35 +1300

    More tests setup

--- src/frames/tests/AllFramesTests.java
+++ src/frames/tests/AllFramesTests.java
@@ -10,5 +10,5 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({})
+@Suite.SuiteClasses({FramesTests.class})
 public class AllFramesTests {}
---
+++ src/gfx/tests/LoadingTests.java
@@ -0,0 +1,17 @@
+package gfx.tests;
+
+/*
+ * SWEN 222 Group Project
+ * Liam Byrne (byrneliam2)
+ * 300338518
+ */
+
+import org.junit.Test;
+
+public class LoadingTests {
+
+    @Test
+    public void test01_ATest() {
+
+    }
+}
