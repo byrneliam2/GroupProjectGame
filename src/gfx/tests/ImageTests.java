8cab1b4bc2c860875154cdf1e16f7c93fff888fd
Liam <liam.jb1@gmail.com>
2017-09-25 22:38:28 +1300

    Image tests

--- src/gfx/tests/ImageTests.java
+++ src/gfx/tests/ImageTests.java
@@ -9,12 +9,17 @@ package gfx.tests;
 import gfx.ImageLoader;
 import gfx.ImageUtilities;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
-import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertTrue;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ImageTests {
@@ -22,16 +27,25 @@ public class ImageTests {
     @Test
     public void test01_Rotation() {
         BufferedImage img = ImageLoader.image("ui", "logo", true);
-        BufferedImage rot90 = ImageUtilities.rotate(img, 90);
-        BufferedImage rotated = ImageUtilities.rotate(rot90, 270);
-        /*byte[] arrRot = ((DataBufferByte) rotated.getData().getDataBuffer()).getData();
-        byte[] arrImg = ((DataBufferByte) img.getData().getDataBuffer()).getData();
-        assertArrayEquals(arrRot, arrImg);*/
-        for (int i = 0; i < img.getHeight(); i++)
-            for (int j = 0; j < img.getWidth(); j++) {
-                if (img.getRGB(i, j) != rotated.getRGB(i, j)) {
-                    fail("Not equal images!");
-                }
-            }
+        BufferedImage rotated = ImageUtilities.rotate(ImageUtilities.rotate(img, 90), 270);
+
+        DataBuffer dbActual = rotated.getRaster().getDataBuffer();
+        DataBuffer dbExpected = img.getRaster().getDataBuffer();
+        DataBufferByte dbiActual = (DataBufferByte) dbActual;
+        DataBufferByte dbiExpected = (DataBufferByte) dbExpected;
+
+        for (int bank = 0; bank < dbiActual.getNumBanks(); bank++) {
+            byte[] actual = dbiActual.getData(bank);
+            byte[] expected = dbiExpected.getData(bank);
+            assertTrue(Arrays.equals(actual, expected));
+        }
+    }
+
+    @Test
+    public void test02_Scale() {
+        BufferedImage img = ImageLoader.image("ui", "logo", true);
+        BufferedImage scaled = ImageUtilities.scale(img, 200, 200);
+        assertTrue(scaled.getWidth() == 200);
+        assertTrue(scaled.getHeight() == 200);
     }
 }
