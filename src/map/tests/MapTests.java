6e7aa51ffdf238d7122841b1a64ecfce6f7f795a
Thomas Edwards <tomo12321@hotmail.com>
2017-09-27 17:38:17 +1300

    added extra null parameter

--- src/map/tests/MapTests.java
+++ src/map/tests/MapTests.java
@@ -10,12 +10,12 @@ public class MapTests {
 
 	@Test
 	public void testCanMove() {
-		Map m = new Map("map1", null, null, null);
+		Map m = new Map("map1", null, null, null, null);
 
 		assertFalse(m.canMove(0, 0));
 		assertFalse(m.canMove(1, 1));
 		assertFalse(m.canMove(5, 5));
-		assertFalse(m.canMove(Map.tileSize-1, Map.tileSize-1));
+		assertFalse(m.canMove(Map.tileSize - 1, Map.tileSize - 1));
 		assertTrue(m.canMove(Map.tileSize + 2, Map.tileSize + 2));
 	}
 
