522106c3757b7851ab43d186af3b4ff2c21a29e2
McManaway1 <andrew.mcmanaway@gmail.com>
2017-09-27 16:04:00 +1300

    Added support for resetting keybinds

--- src/controller/tests/ControllerTests.java
+++ src/controller/tests/ControllerTests.java
@@ -21,63 +21,55 @@ public class ControllerTests {
     //KeyboardCommands.class Tests
 
     /**
-     * Testing Getter methods on keybinds
+     * Testing Keybinds getters and Setters
      */
 
     @Test
-    public void test01_getKeybind_UP(){
+    public void test01_keyUp(){
+        //Getter
         assertEquals(KeyboardCommands.KEY_UP.getKeybind(), KeyEvent.VK_UP);
-    }
-
-    @Test
-    public void test02_getKeybind_DOWN(){
-        assertEquals(KeyboardCommands.KEY_DOWN.getKeybind(), KeyEvent.VK_DOWN);
-    }
-
-    @Test
-    public void test03_getKeybind_LEFT(){
-        assertEquals(KeyboardCommands.KEY_LEFT.getKeybind(), KeyEvent.VK_LEFT);
-    }
-
-    @Test
-    public void test04_getKeybind_RIGHT(){
-        assertEquals(KeyboardCommands.KEY_RIGHT.getKeybind(), KeyEvent.VK_RIGHT);
-    }
-
-    /**
-     * Testing the Setting of Keybinds
-     */
-
-    @Test
-    public void test05_setKeybind_UP(){
-        assertEquals(KeyboardCommands.KEY_UP.getKeybind(), KeyEvent.VK_UP);
+        //Setter
         KeyboardCommands.KEY_UP.setKeybind(KeyEvent.VK_W);
-        assertNotEquals(KeyboardCommands.KEY_UP.getKeybind(), KeyEvent.VK_UP);
         assertEquals(KeyboardCommands.KEY_UP.getKeybind(), KeyEvent.VK_W);
+        //Reset
+        KeyboardCommands.KEY_UP.resetKeybind();
+        assertEquals(KeyboardCommands.KEY_UP.getKeybind(), KeyEvent.VK_UP);
     }
 
     @Test
-    public void test06_setKeybind_DOWN(){
+    public void test02_keyDown(){
+        //Getter
         assertEquals(KeyboardCommands.KEY_DOWN.getKeybind(), KeyEvent.VK_DOWN);
-        KeyboardCommands.KEY_DOWN.setKeybind(KeyEvent.VK_S);
-        assertNotEquals(KeyboardCommands.KEY_DOWN.getKeybind(), KeyEvent.VK_DOWN);
-        assertEquals(KeyboardCommands.KEY_DOWN.getKeybind(), KeyEvent.VK_S);
+        //Setter
+        KeyboardCommands.KEY_DOWN.setKeybind(KeyEvent.VK_W);
+        assertEquals(KeyboardCommands.KEY_DOWN.getKeybind(), KeyEvent.VK_W);
+        //Reset
+        KeyboardCommands.KEY_DOWN.resetKeybind();
+        assertEquals(KeyboardCommands.KEY_DOWN.getKeybind(), KeyEvent.VK_DOWN);
     }
 
     @Test
-    public void test07_setKeybind_LEFT(){
+    public void test03_keyLeft(){
+        //Getter
         assertEquals(KeyboardCommands.KEY_LEFT.getKeybind(), KeyEvent.VK_LEFT);
-        KeyboardCommands.KEY_LEFT.setKeybind(KeyEvent.VK_A);
-        assertNotEquals(KeyboardCommands.KEY_LEFT.getKeybind(), KeyEvent.VK_LEFT);
-        assertEquals(KeyboardCommands.KEY_LEFT.getKeybind(), KeyEvent.VK_A);
+        //Setter
+        KeyboardCommands.KEY_LEFT.setKeybind(KeyEvent.VK_W);
+        assertEquals(KeyboardCommands.KEY_LEFT.getKeybind(), KeyEvent.VK_W);
+        //Reset
+        KeyboardCommands.KEY_LEFT.resetKeybind();
+        assertEquals(KeyboardCommands.KEY_LEFT.getKeybind(), KeyEvent.VK_LEFT);
     }
 
     @Test
-    public void test08_setKeybind_RIGHT(){
+    public void test04_keyRight(){
+        //Getter
         assertEquals(KeyboardCommands.KEY_RIGHT.getKeybind(), KeyEvent.VK_RIGHT);
-        KeyboardCommands.KEY_RIGHT.setKeybind(KeyEvent.VK_D);
-        assertNotEquals(KeyboardCommands.KEY_RIGHT.getKeybind(), KeyEvent.VK_RIGHT);
-        assertEquals(KeyboardCommands.KEY_RIGHT.getKeybind(), KeyEvent.VK_D);
+        //Setter
+        KeyboardCommands.KEY_RIGHT.setKeybind(KeyEvent.VK_W);
+        assertEquals(KeyboardCommands.KEY_RIGHT.getKeybind(), KeyEvent.VK_W);
+        //Reset
+        KeyboardCommands.KEY_RIGHT.resetKeybind();
+        assertEquals(KeyboardCommands.KEY_RIGHT.getKeybind(), KeyEvent.VK_RIGHT);
     }
 
     //MousePosition.class Tests
@@ -87,14 +79,14 @@ public class ControllerTests {
      */
 
     @Test
-    public void test09_initMouse(){
+    public void test05_initMouse(){
         MousePosition mouse = new MousePosition();
         assertTrue(mouse.getX() == 0);
         assertTrue(mouse.getY() == 0);
     }
 
     @Test
-    public void test10_settingMousePosition(){
+    public void test06_settingMousePosition(){
         MousePosition mouse = new MousePosition();
         MouseEvent e = new MouseEvent(new JComponent(){}, 0, 0, 0, 100, 100, 0, false);
         mouse.mouseMoved(e);
@@ -103,7 +95,7 @@ public class ControllerTests {
     }
 
     @Test
-    public void test11_settingMousePosition(){
+    public void test07_settingMousePosition(){
         MousePosition mouse = new MousePosition();
         MouseEvent e = new MouseEvent(new JComponent(){}, 0, 0, 0, 500, 100, 0, false);
         mouse.mouseMoved(e);
@@ -118,43 +110,43 @@ public class ControllerTests {
      */
 
     @Test
-    public void test12_moveUp() {
+    public void test08_moveUp() {
         MousePosition mouse = new MousePosition();
         MockGame game = new MockGame() {
             public boolean movePlayer(int dx, int dy) throws InvalidPlayerExceptions { return dy < 0; }
         };
         Controller c = new Controller(game, mouse);
-        assertTrue(c.processInput(KeyEvent.VK_W, true));
+        assertTrue(c.processInput(KeyEvent.VK_UP, true));
     }
 
     @Test
-    public void test13_moveDown() {
+    public void test09_moveDown() {
         MousePosition mouse = new MousePosition();
         MockGame game = new MockGame() {
             public boolean movePlayer(int dx, int dy) throws InvalidPlayerExceptions { return dy > 0; }
         };
         Controller c = new Controller(game, mouse);
-        assertTrue(c.processInput(KeyEvent.VK_S, true));
+        assertTrue(c.processInput(KeyEvent.VK_DOWN, true));
     }
 
     @Test
-    public void test14_moveLeft() {
+    public void test10_moveLeft() {
         MousePosition mouse = new MousePosition();
         MockGame game = new MockGame() {
             public boolean movePlayer(int dx, int dy) throws InvalidPlayerExceptions { return dx < 0; }
         };
         Controller c = new Controller(game, mouse);
-        assertTrue(c.processInput(KeyEvent.VK_A, true));
+        assertTrue(c.processInput(KeyEvent.VK_LEFT, true));
     }
 
     @Test
-    public void test15_moveRight() {
+    public void test11_moveRight() {
         MousePosition mouse = new MousePosition();
         MockGame game = new MockGame() {
             public boolean movePlayer(int dx, int dy) throws InvalidPlayerExceptions { return dx > 1; }
         };
         Controller c = new Controller(game, mouse);
-        assertTrue(c.processInput(KeyEvent.VK_D, true));
+        assertTrue(c.processInput(KeyEvent.VK_RIGHT, true));
     }
 
 
