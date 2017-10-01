f41c0d5d1f35fc16565b66c712ce2232fb626ef9
McManaway1 <andrew.mcmanaway@gmail.com>
2017-10-01 13:11:47 +1300

    Fixed: Controller shouldn't un-pause the game in the menus

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -11,64 +11,61 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Controller implements IController {
-	private IGame game;
-	private Set<Command> currentCommands;
-	private MouseListener mouse;
+    private IGame game;
+    private Set<Command> currentCommands;
+    private MouseListener mouse;
 
-	public Controller(IGame game, KeyListener keyboard, MouseListener mouse) {
-		this.game = game;
-		this.mouse = mouse;
-		this.currentCommands = new HashSet<>();
+    public Controller(IGame game, KeyListener keyboard, MouseListener mouse) {
+        this.game = game;
+        this.mouse = mouse;
+        this.currentCommands = new HashSet<>();
 
-		keyboard.setController(this);
-		mouse.setController(this);
+        keyboard.setController(this);
+        mouse.setController(this);
 
-		new Timer(MainDisplay.FRAMERATE, (e) -> update()).start();
-	}
+        new Timer(MainDisplay.FRAMERATE, (e) -> update()).start();
+    }
 
-	void notifyCommands(Command cmd, boolean pressed) {
-		if (pressed)
-			currentCommands.add(cmd);
-		else
-			currentCommands.remove(cmd);
-	}
+    void notifyCommands(Command cmd, boolean pressed) {
+        if (pressed)
+            currentCommands.add(cmd);
+        else
+            currentCommands.remove(cmd);
+    }
 
-	private void processCommand(Command cmd) {
-		try {
-			if (!game.isPaused()) {
-				switch (cmd) {
-				case MOVE_UP:
-					game.movePlayer(Direction.N);
-					break;
-				case MOVE_LEFT:
-					game.movePlayer(Direction.W);
-					break;
-				case MOVE_DOWN:
-					game.movePlayer(Direction.S);
-					break;
-				case MOVE_RIGHT:
-					game.movePlayer(Direction.E);
-					break;
-				case INTERACT:
-					game.interact();
-					break;
-				case SHOOT:
-					game.shoot(mouse.getX(), mouse.getY());
-					break;
-				case PAUSE:
-					game.pauseGame();
-					break;
-				}
-			} else if (cmd == Command.PAUSE)
-				game.unPauseGame();
+    private void processCommand(Command cmd) {
+        try {
+            if (!game.isPaused()) {
+                switch (cmd) {
+                    case MOVE_UP:
+                        game.movePlayer(Direction.N);
+                        break;
+                    case MOVE_LEFT:
+                        game.movePlayer(Direction.W);
+                        break;
+                    case MOVE_DOWN:
+                        game.movePlayer(Direction.S);
+                        break;
+                    case MOVE_RIGHT:
+                        game.movePlayer(Direction.E);
+                        break;
+                    case INTERACT:
+                        game.interact();
+                        break;
+                    case SHOOT:
+                        game.shoot(mouse.getX(), mouse.getY());
+                        break;
+                    case PAUSE:
+                        game.pauseGame();
+                        break;
+                }
+            }
+        } catch (InvalidPlayerExceptions ignored) { }
+    }
 
-		} catch (InvalidPlayerExceptions ignored) {
-		}
-	}
-
-	@Override
-	public void update() {
-		if (!game.isPaused())
-			currentCommands.forEach(this::processCommand);
-	}
+    @Override
+    public void update() {
+        if (!game.isPaused())
+            currentCommands.forEach(this::processCommand);
+    }
 }
