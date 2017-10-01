ebc40b97ec38fca7d856b32bb66847541313f9db
Liam <liam.jb1@gmail.com>
2017-10-01 20:32:54 +1300

    Map card testing

--- src/frames/tests/MapCardTests.java
+++ src/frames/tests/MapCardTests.java
@@ -12,13 +12,21 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import javax.swing.*;
+
+import static org.junit.Assert.assertNotNull;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MapCardTests {
 
     @Test
     public void test01_MapData() {
         Game g = new Game();
+        g.newGame();
         MapCard m = new MapCard("map", g.getWorld().getStartingMap(), g);
+        assertNotNull(m.getComponentAt(
+                SwingUtilities.convertPoint(
+                        m.getComponent(0), g.getPlayer().getxLocation(), g.getPlayer().getyLocation(), m)));
     }
 
 }
