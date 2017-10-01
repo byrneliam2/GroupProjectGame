db377565f5b6515271dfd493f69010a3114e9deb
Thomas Edwards <tomo12321@hotmail.com>
2017-10-01 18:47:30 +1300

    added comment

--- src/game/Game.java
+++ src/game/Game.java
@@ -116,8 +116,9 @@ public class Game extends Observable implements IGame, Serializable {
 	 * @see player.Player#move(int dx, int dy)
 	 */
 	public void movePlayer(Direction dir) throws InvalidPlayerExceptions {
+		//if the movement caused a change in maps... notify observers
 		if (player.move(player.getSpeed() * dir.getX(), player.getSpeed() * dir.getY())) {
-			set(player.getMap().getMapName());
+			set(getCurrentMap());
 		}
 	}
 
