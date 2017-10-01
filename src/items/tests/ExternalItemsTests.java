5b6a622bd4609bf5bf31a7298d48b7a158bad71f
Liam <liam.jb1@gmail.com>
2017-10-02 08:31:05 +1300

    Create external test files for items

---
+++ src/items/tests/ExternalItemsTests.java
@@ -0,0 +1,21 @@
+package items.tests;
+
+/*
+ * SWEN 222 Group Project
+ * Liam Byrne (byrneliam2)
+ * 300338518
+ */
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ExternalItemsTests {
+
+    @Test
+    public void test01_ATest() {
+
+    }
+
+}
