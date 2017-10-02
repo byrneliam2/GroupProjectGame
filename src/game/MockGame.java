d9bc86235091540157793d75bcbce08f63bf1bdd
Liam <liam.jb1@gmail.com>
2017-10-02 21:11:14 +1300

    Reformat MockGame

--- src/game/MockGame.java
+++ src/game/MockGame.java
@@ -18,125 +18,128 @@ import java.util.Observer;
 
 /**
  * A mock game class to use for testing.
- * 
+ *
  * @author Thomas Edwards
  * @author Liam Byrne
  */
 public class MockGame implements IGame {
-	private boolean paused = false;
 
-	public MockGame() {
-		new World(new HashMap<>());
-	}
+    private boolean paused = false;
 
-	/******************* View Methods **********************/
+    public MockGame() {
+        new World(new HashMap<>());
+    }
 
-	/**
-	 * @return empty string.
-	 */
-	public String getCurrentMap() {
-		return "";
-	}
+    /******************* View Methods **********************/
 
-	/**
-	 * @return new nullified player
-	 */
-	public Player getPlayer() {
-		return new Player("", 0, 0);
-	}
+    /**
+     * @return empty string.
+     */
+    public String getCurrentMap() {
+        return "";
+    }
 
-	@Override
-	public boolean isOver() {
-		return false;
-	}
+    /**
+     * @return new nullified player
+     */
+    public Player getPlayer() {
+        return new Player("", 0, 0);
+    }
 
-	/**
-	 * @return game world
-	 */
-	public World getWorld() {
-		return new World(new HashMap<>());
-	}
+    @Override
+    public boolean isOver() {
+        return false;
+    }
 
-	/**
-	 * @return HashMap of item->Point(x,y)
-	 */
-	public HashMap<Item, Point> getItems() {
-		return new HashMap<>();
-	}
+    /**
+     * @return game world
+     */
+    public World getWorld() {
+        return new World(new HashMap<>());
+    }
 
-	/**
-	 * @return List of all npc's, each npc has an x,y location similar to the player.
-	 */
-	public List<NPC> getNPCs() {
-		return new ArrayList<>();
-	}
+    /**
+     * @return HashMap of item->Point(x,y)
+     */
+    public HashMap<Item, Point> getItems() {
+        return new HashMap<>();
+    }
 
-	/**
-	 * @return List of all bullets in game.
-	 */
-	public List<Bullet> getBullets() {
-		return Bullet.bulletList;
-	}
+    /**
+     * @return List of all npc's, each npc has an x,y location similar to the player.
+     */
+    public List<NPC> getNPCs() {
+        return new ArrayList<>();
+    }
 
-	public List<Item> getInventory() {
-		return new ArrayList<>();
-	}
+    /**
+     * @return List of all bullets in game.
+     */
+    public List<Bullet> getBullets() {
+        return Bullet.bulletList;
+    }
 
-	public List<Equipable> getEquippedItems() {
-		return new ArrayList<>();
-	}
+    public List<Item> getInventory() {
+        return new ArrayList<>();
+    }
 
-	public void movePlayer(Direction dir) throws InvalidPlayerExceptions {
+    public List<Equipable> getEquippedItems() {
+        return new ArrayList<>();
+    }
 
-	}
+    public void movePlayer(Direction dir) throws InvalidPlayerExceptions {
 
-	public void interact() throws InvalidPlayerExceptions {
-	}
+    }
 
-	@Override
-	public void shoot(double x, double y) {
+    public void interact() throws InvalidPlayerExceptions {
+    }
 
-	}
+    @Override
+    public void shoot(double x, double y) {
 
-	public void dropItem(Item i) throws InvalidPlayerExceptions {
-	}
+    }
 
-	public void equipItem(Equipable i) throws InvalidPlayerExceptions {
-	}
+    public void dropItem(Item i) throws InvalidPlayerExceptions {
+    }
 
-	public void unequipItem(Equipable i) throws InvalidPlayerExceptions {
-	}
+    public void equipItem(Equipable i) throws InvalidPlayerExceptions {
+    }
 
-	public void useItem(Usable u) throws InvalidPlayerExceptions {
-	}
+    public void unequipItem(Equipable i) throws InvalidPlayerExceptions {
+    }
 
-	public void shoot(double direction) throws InvalidPlayerExceptions {
-	}
+    public void useItem(Usable u) throws InvalidPlayerExceptions {
+    }
 
-	@Override
-	public void giveObserver(Observer o) {}
+    public void shoot(double direction) throws InvalidPlayerExceptions {
+    }
 
-	@Override
-	public void set(Object arg) {}
+    @Override
+    public void giveObserver(Observer o) {
+    }
 
-	@Override
-	public void newGame() {
+    @Override
+    public void set(Object arg) {
+    }
 
-	}
+    @Override
+    public void newGame() {
 
-	public void pauseGame() {
-		paused = true;
-	}
+    }
 
-	public void unPauseGame() {
-		paused = false;
-	}
+    public void pauseGame() {
+        paused = true;
+    }
 
-	@Override
-	public boolean isPaused() {
-		return paused;
-	}
+    public void unPauseGame() {
+        paused = false;
+    }
 
-	public void saveGame() {
-	}
+    @Override
+    public boolean isPaused() {
+        return paused;
+    }
+
+    public void saveGame() {
+    }
 }
