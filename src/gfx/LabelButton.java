51f9ee159a2a69231592027f0b3a0c738df28e1e
McManaway1 <andrew.mcmanaway@gmail.com>
2017-10-05 14:56:40 +1300

    LabelButton: Implemented Label Buttons

--- src/gfx/LabelButton.java
+++ src/gfx/LabelButton.java
@@ -9,50 +9,58 @@ public class LabelButton extends JButton {
     private String text;
     private final int WIDTH, HEIGHT;
 
-    public LabelButton(String text, int width, int height, boolean rollover) {
+    public LabelButton(String text, int width, int height) {
         this.setContentAreaFilled(false);
         this.setBorderPainted(false);
         this.setAlignmentX(0.5f);
         this.text = text;
         this.WIDTH = width;
         this.HEIGHT = height;
-        this.createImages(rollover);
+
+        this.createImages();
     }
 
-    private void createImages(boolean rollover){
-        BufferedImage main = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = main.createGraphics();
-        RenderingHints rh = new RenderingHints(
-                RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-        g2d.setRenderingHints(rh);
-        g2d.setStroke(new BasicStroke(2));
-        g2d.setColor(Color.white);
-        g2d.drawRect(0,0,WIDTH,HEIGHT);
-
+    private void createImages(){
+        /* Image Setup */
+        BufferedImage normal = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage clicked = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage rollover = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         Font font = new Font("Gill Sans MT Condensed", Font.BOLD, 30);
+        Graphics2D g2d = normal.createGraphics();
         FontMetrics metrics = g2d.getFontMetrics(font);
-        g2d.setFont(font);
-
+        /* Getting Font Sizes */
         int x = (WIDTH - metrics.stringWidth(this.text)) / 2;
         int y = ((HEIGHT - metrics.getHeight()) / 2) + metrics.getAscent();
 
+        /* Create Normal Button*/
+        g2d.setRenderingHints(rh);
+        g2d.setFont(font);
+        g2d.setColor(Color.white);
+
+        g2d.drawImage(ImageLoader.image("ui", "Button", true), 0, 0, WIDTH, HEIGHT, null);
         g2d.drawString(this.text, x, y);
-        this.setIcon(new ImageIcon(main));
+        this.setIcon(new ImageIcon(normal));
 
-        if (rollover) {
-            BufferedImage roll = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-            g2d = roll.createGraphics();
-            g2d.setRenderingHints(rh);
-            g2d.setColor(Color.white);
-            g2d.fillRect(0,0,WIDTH,HEIGHT);
+        /* Create Clicked Button */
+        g2d = (Graphics2D) clicked.getGraphics();
+        g2d.setRenderingHints(rh);
+        g2d.setFont(font);
+        g2d.setColor(Color.white);
 
-            g2d.setColor(Color.BLACK);
-            g2d.setFont(font);
-            g2d.drawString(this.text, x, y);
-            this.setRolloverEnabled(true);
-            this.setRolloverIcon(new ImageIcon(roll));
-        }
+        g2d.drawImage(ImageLoader.image("ui", "Clicked", true), 0, 0, WIDTH, HEIGHT, null);
+        g2d.drawString(this.text, x, y);
+        this.setPressedIcon(new ImageIcon(clicked));
+
+        /* Create Rollover Button */
+        g2d = (Graphics2D) rollover.getGraphics();
+        g2d.setRenderingHints(rh);
+        g2d.setFont(font);
+        g2d.setColor(Color.white);
+
+        g2d.drawImage(ImageLoader.image("ui", "Rollover", true), 0, 0, WIDTH, HEIGHT, null);
+        g2d.drawString(this.text, x, y);
+        this.setRolloverEnabled(true);
+        this.setRolloverIcon(new ImageIcon(rollover));
     }
 }
