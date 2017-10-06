9bc98c10c193b9145b21a962856c5dc387f2975a
byrneliam2 <byrneliam2>
2017-10-06 15:17:46 +1300

    Move constants out into new common class

---
+++ src/common/DisplayValues.java
@@ -0,0 +1,15 @@
+package common;
+
+/*
+ * SWEN 222 Group Project
+ * Liam Byrne (byrneliam2)
+ * 300338518
+ */
+
+public class DisplayValues {
+
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
+    public static final int FRAMERATE = 1000/60;
+
+}
