79139e825e1a3145a58df636c7be1c04d7e5d1f7
McManaway1 <andrew.mcmanaway@gmail.com>
2017-10-06 23:22:56 +1300

    External-Testing: Created NPC Test Class

--- src/npc/tests/AllTests.java
+++ src/npc/tests/AllTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ DirectionTests.class, PatrolNPCTests.class, })
+@Suite.SuiteClasses({ NPCTests.class, DirectionTests.class, PatrolNPCTests.class, })
 public class AllTests {
 }
---
+++ src/npc/tests/NPCTests.java
@@ -0,0 +1,19 @@
+package npc.tests;
+
+import org.junit.Test;
+
+public class NPCTests {
+
+
+
+
+
+
+
+
+    /* EXTERNAL TESTING */
+    @Test
+    public void testTakeDamage(){
+
+    }
+}
