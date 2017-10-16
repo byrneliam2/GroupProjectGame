3b394c48571357c3e6ffd5b1c7addf43a4d792e6
Mohsen javaher <mohse.javaher@gmail.com>
2017-10-16 19:38:09 +1300

    tests

--- src/player/tests/PlayerTest.java
+++ src/player/tests/PlayerTest.java
@@ -92,18 +92,23 @@ public class PlayerTest {
 
 	/**
 	 * Check if the defaultFireRate of the player decreases when the Player picks up a gun.
+	 * @throws InvalidBackpackException
 	 */
 	@Test
-	public void testDefaultFireRate() {
-		// Create a Player with specific coordinates
-		// Create a gun Item with specific coordinates next to the player
-		// player needs to pick up the gun
+	public void testDefaultFireRate() throws InvalidBackpackException {
+		player = new Player(name, xLocation, yLocation);
+		// insert an item with specific coordinate next to the player
+		Item i = new MassiveGun();
+		m.placeItem(i, 95, 95);
+		// use the pick up item from the player class
+		double fireRateTemp = player.getDefaultFireRate();
+		player.getBackpack().pickUpItem(i);
 		// check if the player backpack contatins the gun
+		if (player.getBackpack().getInventory().contains(i)) {
 			// if it does contain the gun
-				//if the fire rate should have increased
-					// assertTrue
-				// else
-					//assertFalse
+			//if the fire rate should have increased assertTrue
+			assertTrue((fireRateTemp<player.getFireRate()));
+		}
 	}
 
 	/**
@@ -113,6 +118,7 @@ public class PlayerTest {
 	 */
 	@Test
 	public void testTakeDamage() {
+
 		//
 	}
 
@@ -131,5 +137,24 @@ public class PlayerTest {
 	@Test
 	public void testCanMove() {
 
+	}
+
+	/**
+	 * Check if the player can not walk through the walls and the doors if the player doesnt have the key.
+	 */
+	@Test
+	public void tesDeadthPlayer() {
+		// insert a player with specific coordinate
+		player = new Player(name, xLocation, yLocation);
+		// initially player has 5 hearts
+		for(int i =0; i<5; i++) {
+			// attack the player for 5 times
+			player.takeDamage();
+			// check if the the player heart is equall to zero
+			if(player.getHealth() == 0) {
+				// then the isDead flag for the player has turned to true
+				assertTrue(player.isDead());
+			}
+		}
 	}
 }
